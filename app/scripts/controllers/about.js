854a7014615e3b857d6ea6017c2305126f31a580
beginnerhk <learn_angular@yahoo.co.jp>
2015-07-19 21:56:58 +0900

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testdirApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testdirApp
+ */
+angular.module('testdirApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testdirApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the testdirApp
+ */
+angular.module('testdirApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
